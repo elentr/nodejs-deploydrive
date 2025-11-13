70cdc973c02eb13865b2011e147ebeacaf9f5f04
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-11-13 10:44:51 +0100

    fix: swagger

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,34 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    files: ["**/*.{js,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.node,
+    },
+    plugins: {
+      js,
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      prettier,
+    ],
+
+    rules: {
+      "prettier/prettier": "warn",
+    },
   },
-  tseslint.configs.recommended,
 ]);
+
